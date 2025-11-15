c0795eb12356944b7429bcdd9307068a8efd04aa
Ketsu Blank <ketsublank@gmail.com>
2025-11-15 11:25:58 +0200

    Замена gulp и package

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
-const minify = require('gulp-minify');
+const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 
+// Объединение и минификация CSS
 gulp.task('styles', function() {
     return gulp.src([
-        'src/css/components/*.css',
         'src/css/style.css'
     ])
     .pipe(concat('all.min.css'))
@@ -15,27 +15,21 @@ gulp.task('styles', function() {
     .pipe(gulp.dest('dist/css'));
 });
 
+// Объединение и минификация JS
 gulp.task('scripts', function() {
     return gulp.src([
-        'src/js/modules/*.js',
         'src/js/main.js'
     ])
     .pipe(concat('all.min.js'))
-    .pipe(minify())
+    .pipe(uglify())
     .pipe(gulp.dest('dist/js'));
 });
 
+// Копирование HTML
 gulp.task('html', function() {
     return gulp.src('src/*.html')
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('src/css/**/*.css', gulp.series('styles'));
-    gulp.watch('src/js/**/*.js', gulp.series('scripts'));
-    gulp.watch('src/*.html', gulp.series('html'));
-});
-
+// Сборка проекта
 gulp.task('build', gulp.parallel('styles', 'scripts', 'html'));
-
-gulp.task('default', gulp.series('build', 'watch'));
